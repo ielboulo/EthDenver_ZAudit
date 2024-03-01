9bf1bfa8cbb8408b6f3fae7dcca34911a1412446
emanherawy <eman.herawy@gmail.com>
2024-03-01 20:13:33 +0200

    update hh config file to support  dotenv and  update networks

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -6,14 +6,16 @@ import "hardhat-docgen";
 import "hardhat-gas-reporter";
 import "./tasks/accounts";
 import "@nomiclabs/hardhat-etherscan";
-const fs = require('fs');
-const privateKey = fs.readFileSync("privateKey.secret").toString().trim();
-const alchemyKey = fs.readFileSync("alchemyKey.secret").toString().trim();
-const config: HardhatUserConfig = {
+import 'dotenv/config'
+
+ const privateKey = process.env.PRIVATE_KEY;
+
+ const config: HardhatUserConfig = {
   namedAccounts: {
     deployer: 0,
   },
   defaultNetwork: 'hardhat',
+
   networks: {
     hardhat: {
       saveDeployments: true,
@@ -24,87 +26,53 @@ const config: HardhatUserConfig = {
       chainId: 1337,
       saveDeployments: true,
     },
-
+zircuit: {
+      url: `https://zircuit1.p2pify.com`,
+      accounts: [privateKey],
+      saveDeployments: true,
+    },
+    scroll: {
+      url: 'https://scroll-sepolia.blockpi.network/v1/rpc/public',
+      accounts: [privateKey || ''],
+      saveDeployments: true,
+    },
+    linea: {
+      url: 'https://rpc.goerli.linea.build',
+      accounts: [privateKey || ''],
+      saveDeployments: true,
+    },
+    matis_main: {
+      url: 'https://metis-pokt.nodies.app',
+      accounts: [privateKey || ''],
+      saveDeployments: true,
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/tCbwTAqlofFnmbVORepuHNcsrjNXWdRJ',
+      accounts: [privateKey || ''],
+      saveDeployments: true,
+    },
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      accounts: [privateKey || ''],
+      saveDeployments: true,
+    },     calibrationnet: {
+      chainId: 314159,
+      url: "https://api.calibration.node.glif.io/rpc/v1",
+      accounts: [privateKey],
+  },
+  filecoinmainnet: {
+      chainId: 314,
+      url: "https://api.node.glif.io",
+      accounts: [privateKey],
+  },
     testnet_aurora: {
       url: 'https://testnet.aurora.dev',
       accounts: [privateKey],
       saveDeployments: true,
       chainId: 1313161555,
       gasPrice: 120 * 1000000000,
-    },
-    bsc_test: {
-      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
-      accounts: [privateKey],
-
-      saveDeployments: true,
-      chainId: 97,
-     },
-    bsc: {
-      url: 'https://bsc-dataseed1.binance.org',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 56,
-      gasPrice: 5 * 1000000000,
-    },
-    xDai: {
-      url: 'https://rpc.xdaichain.com',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 0x64,
-      gasPrice: 120 * 1000000000,
-    },
-    FTM: {
-      url: 'https://rpc.fantom.network',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 250,
-      gasPrice: 120 * 1000000000,
-    },
-    heco: {
-      url: 'https://http-mainnet.hecochain.com/',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 128,
-    },
-    heco_test: {
-      url: 'https://http-testnet.hecochain.com',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 256,
-    },
-    matic_test: {
-      url: 'https://matic-mumbai.chainstacklabs.com/',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 80001,
-      gasPrice: 120 * 1000000000,
-    },
-    matic: {
-      url: 'https://rpc-mainnet.matic.network',
-      accounts: [privateKey],
-      saveDeployments: true,
-      chainId: 137,
-      gasPrice: 120 * 1000000000,
-    },
-
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${alchemyKey}`,
-      saveDeployments: true,
-      accounts: [privateKey],
-    },
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${alchemyKey}`,
-      saveDeployments: true,
-      accounts: [privateKey],
-      loggingEnabled: true,
-      gasPrice: 80 * 1000000000,
-      throwOnTransactionFailures: true,
-    },
-    rinkeby: {
-      url: `https://eth-rinkeby.alchemyapi.io/v2/${alchemyKey}`,
-      saveDeployments: true,
-      accounts: [privateKey],
-    },
+    }
+  
   },
   solidity: {
     compilers: [
